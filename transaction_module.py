e32c8d16655fbd3d94208e3f3c8cfc5ccf685fb4
gowthamibk <gowthamibkbsc24@rvu.edu.in>
2026-03-16 12:03:50 +0530

    feat: initialize base ledger module

---
+++ transaction_module.py
@@ -0,0 +1,13 @@
+# Transaction Module
+
+# Take two numbers as input
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate total and difference
+total = num1 + num2
+difference = num1 - num2
+
+# Display results
+print("Total Combined Value:", total)
+print("Difference:", difference)
